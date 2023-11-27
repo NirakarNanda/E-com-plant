799d74fa8ceffdda7a9789aa48d4d42aab389294
Nirakar Nanda <115453242+NirakarNanda@users.noreply.github.com>
2023-11-28 03:52:04 +0530

    added cart promo and Final commit befor deploy

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Navbar.css'
 import { useState } from 'react'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
 
 export const Navbar = () => {
 
     const [menu,setMenu] = useState("SHOP");
+    const {getTotalCartItems} = useContext(ShopContext);
 
   return (
     <div className='navbar'>
@@ -27,7 +29,7 @@ export const Navbar = () => {
         <ul className="nav-login-cart">
             <Link to='/login'><button>Login</button></Link>
             <Link to='/cart'><img src={cart_icon} alt="" /></Link>
-            <div className="nav-cart-count">0</div>
+            <div className="nav-cart-count">{getTotalCartItems()}</div>
         </ul>
     </div>
   )
